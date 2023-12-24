4eeebd4956698913e05b13f952517a4d06f2897d
MarianaSko <maryana.skorynovych@gmail.com>
2023-12-24 13:34:14 +0200

    v-1

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -6,7 +6,6 @@ axios.defaults.baseURL = 'https://65870e91468ef171392f3846.mockapi.io'
 export const fetchContactsThunk = createAsyncThunk('fetchContacts', async (_, thunkApi) => {
     try {
         const { data } = await axios.get('/contacts')
-        console.log(data);
         return data
     } catch (error) {
         return thunkApi.rejectWithValue(error.message)
@@ -25,7 +24,6 @@ export const deleteContactsThunk = createAsyncThunk('deleteContacts', async (id,
 export const addContactsThunk = createAsyncThunk('addContacts', async (contact, thunkApi) => {
     try {
         const { data } = await axios.post('/contacts', contact)
-        console.log(data);
         return data
     } catch (error) {
         return thunkApi.rejectWithValue(error.message)
